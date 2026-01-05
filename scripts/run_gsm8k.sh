2a583418ba08c0ef6b03a0b7774a579e07d189e0
dbtmddn41 <dbtmddn41@gmail.com>
2026-01-05 08:52:01 +0000

    cmt

--- scripts/run_gsm8k.sh
+++ scripts/run_gsm8k.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-#SBATCH -c 8
-#SBATCH -t 600
-#SBATCH -p seas_gpu
-#SBATCH --gres=gpu:1
-#SBATCH --mem=128000
-#SBATCH --open-mode=append
-#SBATCH -o logs/%j.out
-#SBATCH -e logs/%j.err
-#SBATCH --mail-user=sangwonjung@g.harvard.edu
-#SBATCH --mail-type=ALL
-
 module load python/3.10 cudnn cuda/12.4 gcc
 source activate RERD
 
